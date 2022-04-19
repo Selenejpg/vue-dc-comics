866dc5d2f2d08414588dbf6daf8b60dc9f2ef4cd
noemipintaldi <noemipintaldi@gmail.com>
2022-04-19 16:38:23 +0200

    esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,32 @@
       <LogoComp/>
       <NavComp/>
     </header>
+    <div class="container">
+      <img class="jumbo" src="../public/img/jumbotron.jpg" alt="">
+    </div>
     <main>
-      <div class="main-active"><h2>Il contenuto va qui</h2></div>
-      <div class="main-barra"><BarraComp/></div>
+      <div class="margin">
+        <div class="current">
+          <h1>CURRENT SERIES</h1>
+        </div> 
+      </div>
+
+      <div class="products">
+        <ProductComp 
+          v-for="(element, index) in products" 
+          :key="index"
+          :details="element"
+        />
+      </div>
+      <div class="flex">
+        <div class="button">
+          <h1>LOAD MORE</h1>
+        </div> 
+      </div>
+          
+      <div class="main-barra"><BarraComp/></div> 
     </main>
+    
     <FooterComp/>  
     <BarraFooterComp/>   
   </div>
@@ -19,6 +41,7 @@ import NavComp from './components/NavComp.vue'
 import BarraComp from './components/BarraComp.vue'
 import FooterComp from './components/FooterComp.vue'
 import BarraFooterComp from './components/BarraFooterComp.vue'
+import ProductComp from './components/ProductComp.vue'
 
 export default {
   name: 'App',
@@ -27,10 +50,65 @@ export default {
     NavComp,
     BarraComp,
     FooterComp,
-    BarraFooterComp
+    BarraFooterComp,
+    ProductComp
+  },
+  data() {
+    return{
+      products: [
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+          titolo: "Action Comics", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+          titolo: "American Vampire 1976", 
+        }, 
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+          titolo: "Aquaman", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+          titolo: "Batgirl", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          titolo: "Batman", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          titolo: "Batman Beyond", 
+        }, 
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+          titolo: "Batman/Superman", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+          titolo: "Batman/Superman Annual", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+          titolo: "Batman: The Joker War Zone", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+          titolo: "Batman: Three Jokers", 
+        }, 
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+          titolo: "Batman: White Knight Presents: Harley Quinn", 
+        },
+        {
+          src: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+          titolo: "Catwoman", 
+        }
+      ]
+    }
   }
-  
-}
+} 
+
 </script>
 
 <style lang="scss">
@@ -61,19 +139,58 @@ header {
 }
 
 main {
-    width: 100%;
+  width: 100%;
+  background-color: black;
 }
 
-.main-active {
-  background-color: black;
-  color: white;
-  text-align: center;
-  padding: 40px;
+h1 {
+  font-size: 12px;
+}
+
+.products{
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
 }
 
 .main-barra{
   background-color: $blue;
   color: white;
   padding: 50px;
+}
+
+.button{
+  width: 10%;
+  color: white;
+  background-color: $blue;
+  padding: 10px;
+}
+
+.flex{
+  display: flex;
+  justify-content: center;
+  padding-bottom: 30px;
+}
+
+.margin {
+  margin-left: 185px;
+  padding-top: 30px;
+}
+
+.current {
+  width: 10%;
+  color: white;
+  background-color: $blue;
+  padding: 10px;
+}
+
+.jumbo {
+  max-width: 100vw;
+}
+
+.container {
+  height: 60vh;
 }
 </style>
---
+++ src/components/ProductComp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main-active product">
+    <div class="box">
+      <img :src="details.src" alt="">
+      <h3>{{ details.titolo }}</h3>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'ProductComp',
+  props: ['details']
+}
+</script>
+
+<style scoped lang="scss">
+
+@import './style/variabili.scss';
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  .main-active {
+    background-color: black;
+    color: white;
+    text-align: center;
+    padding: 30px;
+  }
+
+.product {
+  display: flex;
+  flex-basis: calc(100% / 6);
+
+  img {
+    width: 100%;
+  }
+
+  h3 {
+    font-size: 12px;
+  }
+}
+
+
+</style>
